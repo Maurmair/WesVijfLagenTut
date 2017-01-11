4608cdbd0367478feb26b64b2cb1f378c041a984
Maurmair <maurmair@gmail.com>
2017-01-11 13:18:23 +0100

    initcomtest sadasd

--- WesVijfLagen/Models/Gebruiker.cs
+++ WesVijfLagen/Models/Gebruiker.cs
@@ -7,7 +7,27 @@ namespace WesVijfLagen.Models
 {
     public partial class Gebruiker
     {
-        
+        public void GeoCacheGevonden(string locatie)
+        {
+            using (TestModelContainer context = new TestModelContainer())
+            {
+                IQueryable<GeoCache> GeoCaches = context.GeoCacheSet;
+                GeoCache[] geoArray = (
+                    from g in GeoCaches
+                    where g.Locatie == locatie
+                    select g).ToArray();
+                
+                if (geoArray.Length > 0)
+                {
+                    context.GeoCacheSet.Add(geoArray[0]);
+                    context.SaveChanges();
+                }
+                else
+                {
+                    new Exception("Locatie niet gevonden");
+                }
+            }
+        }
     }
     
 }
